3df02373da8aeb260ef9e0d3b8b20ff96a9c7386
drewwmercer <awmercer@gmail.com>
2021-02-27 15:43:14 -0500

    Updating data preparation

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask import Flask
 
 app = Flask(__name__)
 
-bootstrap = Bootstrap(app)
+Bootstrap(app)
 
 @app.route('/')
 def hello_world():
---
+++ gfd/autoapp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from gfd.app import create_app
+
+app = create_app()
