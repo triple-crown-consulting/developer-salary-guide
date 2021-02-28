a0aad6c311290330d2551e21ff1bf617ead04a64
drewwmercer <awmercer@gmail.com>
2021-02-28 17:40:11 -0500

    Adding the favicon to correct the 404

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+
+import os # for the favicon
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
