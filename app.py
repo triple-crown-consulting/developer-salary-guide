1b14a8f33d3aaa566819303e08f8c4620db1be12
drewwmercer <awmercer@gmail.com>
2021-03-01 09:33:56 -0500

    Working on authentication

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def advisor():
 def predict():
     int_features = [int(x) for x in request.form.values()]
     final = [np.array(int_features)]
-    prediction = model.predict_salary(final)
+    prediction = model.predict(final)
     output = '{0: {1}f}'.format(prediction[0][1], 2)
 
     return render_template('advise.html', predictiontext='Salary estimator has completed the analysis.\nSuggested Salary is: ${}'.format(output))
